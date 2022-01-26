407d4f6b0fb7529e522aadc2ddc98a44bfd6aa78
Hussein-bc <hussein.akram@bellevuecollege.edu>
2022-01-25 23:06:58 -0800

    Assembly load file fixed

--- BCUnitEngine/Engine.cs
+++ BCUnitEngine/Engine.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using BCUnitFramework;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using BCUnitFramework;
 
 
 // Class Library (.NET Framework)
@@ -11,43 +11,35 @@ using BCUnitFramework;
 
 namespace BCUnitEngine
 {
-
     public class Engine
     {
 
-
         private string path;
         private List<Type> AllAtributes = null;
-        private readonly string fileType = "*.dll";
-
         private List<string> invokedMethodNames = new List<string>();
 
-        public Engine (string path)
+        public Engine(string path)
         {
-            this.path = path + @"\bin";
-            ShowMessage(path);
-            
+            this.path = path;
+
             Go();
             ShowMessage("END OF ENGINE");
         }
 
         private void Go()
         {
-            try {
+            try
+            {
 
-               
-                Directory.SetCurrentDirectory(path);
-                
-                //ShowMessage("Engine SetCurrentDirectory: " + path);
                 AllAtributes = new List<Type>();
 
                 AllAtributes = ReadAllAttribute();
 
                 MethodsParser();
 
-
-
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 ShowMessage("ERROR: " + e.Message);
             }
 
@@ -56,25 +48,26 @@ namespace BCUnitEngine
 
         private void MethodsParser()
         {
-            ShowMessage("method parser");
-            foreach (Type t in AllAtributes) {
+            foreach (Type t in AllAtributes)
+            {
 
-                ShowMessage("method parser before query");
+
                 // Query to find the BeforeAllTestAttribute tag
                 var BeforeAllTest = from m in t.GetMethods()
                                     where m.GetCustomAttributes(false).Any(a => a is BeforeAllTestsAttribute)
                                     select m;
-                ShowMessage("1 before InvokeMethod");
+
                 InvokeMethod(t, BeforeAllTest);
 
 
-                // Query to find the TestMethodsAttriubte tags
+                //    // Query to find the TestMethodsAttriubte tags
                 var allTestMethods = from m in t.GetMethods()
                                      where m.GetCustomAttributes(false).Any(a => a is TestMethodAttribute)
                                      select m;
 
                 // Re-ordering the founded methods
-                var ordered = allTestMethods.OrderBy(p => p.GetCustomAttribute<TestMethodAttribute>().Order);
+                var ordered = allTestMethods.OrderBy(p => p.GetCustomAttribute<
+                    TestMethodAttribute>().Order);
 
                 // Two lists to store the TestMethodsAttriubte tags 
                 List<MethodInfo> withOrder = new List<MethodInfo>();        // has Order = 1, 2, 3, ..
@@ -83,13 +76,16 @@ namespace BCUnitEngine
 
 
                 // adding TestMethodsAttriubte methods to the lists
-                foreach (var v in ordered) {
+                foreach (var v in ordered)
+                {
                     int order = v.GetCustomAttribute<TestMethodAttribute>().Order;
 
-
-                    if (order != 0) {
+                    if (order != 0)
+                    {
                         withOrder.Add(v);
-                    } else {
+                    }
+                    else
+                    {
                         withoutOrder.Add(v);
                     }
                 }
@@ -99,14 +95,12 @@ namespace BCUnitEngine
 
                 InvokeMethod(t, newList);
 
-
                 // Checking for AfterAllTests Attribute tag
                 var afterTest = from m in t.GetMethods()
                                 where m.GetCustomAttributes(false).Any(a => a is AfterAllTestsAttribute)
                                 select m;
 
                 InvokeMethod(t, afterTest);
-
             }
 
         }
@@ -115,80 +109,51 @@ namespace BCUnitEngine
         private void InvokeMethod(Type type, IEnumerable<MethodInfo> testList)
         {
 
-            ShowMessage("inside InvokeMethod ");
-            if (testList != null) {
+            if (testList != null)
+            {
                 object instance = Activator.CreateInstance(type);
-                foreach (MethodInfo mInfo in testList) {
+                foreach (MethodInfo mInfo in testList)
+                {
                     //Console.Write($"Engine is running on {mInfo.Name}() => ");
+                    invokedMethodNames.Add(mInfo.ToString());
 
-                    //ShowMessage($"Engine is running on {mInfo.Name}() => ");
-                    invokedMethodNames.Add(mInfo.Name);
+                    ShowMessage($"Engine is running on {mInfo.Name}() => ");
                     mInfo.Invoke(instance, new object[0]);
-                    
+
                 }
             }
         }
-
-
 
         private List<Type> ReadAllAttribute()
         {
             var pluginList = new List<Type>();
 
+            Assembly assembly = Assembly.LoadFrom(path);
 
-            ShowMessage("1 inside ReadAllAttribute");
-            var files = Directory.GetFiles(Directory.GetCurrentDirectory(), fileType, SearchOption.AllDirectories);
-            //foreach (var file in files)
-            //    ShowMessage("file\n" + file);
+            Type[] allTypes = assembly.GetTypes();
+            foreach (Type type in allTypes)
+            {
 
-            List<Assembly> tempAssemblyList = new List<Assembly>();
-            foreach (var file in files) {
-                
-
-                Assembly assembly = Assembly.LoadFile(Path.Combine(Directory.GetCurrentDirectory(), file));
-                ShowMessage("ass fullname\n" + assembly.FullName);
-                tempAssemblyList.Add(assembly);
-
-            }
-
-            foreach (Assembly a in tempAssemblyList) {
-                Type[] allTypes = a.GetTypes();
-                foreach (Type type in allTypes) {
-                    //ShowMessage("type in temp ass list\n" + type.Name);
-                    if ((type.GetCustomAttribute(typeof(TestClassAttribute))) is TestClassAttribute) {
-                        //ShowMessage("list has:\n" + type);
-                        pluginList.Add(type);
-
-                    }
+                if (((type.GetCustomAttribute(typeof(TestClassAttribute))) is TestClassAttribute))
+                {
+                    pluginList.Add(type);
                 }
             }
-            
+
+
             return pluginList;
-            
+
         }
 
-
-
-
-        public void ShowPath()
-        {
-            System.Windows.Forms.MessageBox.Show($"The path is {path}");
-        }
 
         public void ShowMessage(string s)
         {
             System.Windows.Forms.MessageBox.Show($"Message:\n{s}");
         }
-
         public List<string> GetInvokedMethodNames()
         {
             return invokedMethodNames;
         }
 
-
-        public int GetCount()
-        {
-            return AllAtributes.Count;
-        }
     }
 }
